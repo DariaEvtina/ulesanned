094eb49c6a1abbf6ef1d700b513c27a332b4b0af
opilane <opilane@HP-CZC6297B9M>
2022-10-10 10:12:54 +0300

    uy

---
+++ ulesanned/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ulesanned
+{
+
+    internal class ApplicationContext : DbContext
+    {
+        public string connection()
+        {
+            return @"Data Source=(LocalDB)\\\\MSSQLLocalDB;AttachDbFilename=C:\\\\Users\\\\opilane\\\\source\\\\repos\\\\dariaEvtinaTARpv20\\\\ulesanned\\\\ulesanned\\\\kasutajad.mdf;Integrated Security=True;Connect Timeout=30\";
+        }
+        public DbSet<kasutaja> Logins => Set<kasutaja>();
+        //public ApplicationContext() => Database.EnsureCreated();
+
+        /*protected override void OnConfiguring(DbConfiguration optionsBuilder)
+        {
+            kasutaja kas=new kasutaja();
+            optionsBuilder.UseSqlite(kas.connection);
+        }*/
+    }
+}
---
+++ ulesanned/admin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ulesanned
+{
+    public partial class admin : Form
+    {
+        public admin()
+        {
+            this.Text = "admin vorm";
+            this.Size = new Size(600, 500);
+            TableLayoutPanel tlp = new TableLayoutPanel
+            {
+                Dock = DockStyle.Fill,
+                RowCount = 6,
+                ColumnCount = 2,
+            };
+        }
+
+        private void admin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
